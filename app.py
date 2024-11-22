bee1d008d861abe3a04b0706e2922ad8cc0dcb3f
matheustoledoo <matheustoledo2312@gmail.com>
2024-11-22 15:52:49 -0300

    a

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def adicionar_escopo_dinamicos(slide, lista_escopo):
     width = Inches(1.5)
     height = Inches(0.5)
     espacamento_vertical = Inches(0.9)
-    limite_caracteres = 45 # Limite de caracteres por linha
+    limite_caracteres = 45  # Limite de caracteres por linha
 
     for escopo in lista_escopo:
         textbox = slide.shapes.add_textbox(left, top, width, height)
